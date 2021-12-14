75e99786640fdae5961e3808d40f9e378a578780
LucaVenturi <luca.venturi29@studio.unibo.it>
2021-12-14 15:58:43 +0100

    Implement and Test

--- 2-Exceptions/FixedSizeQueue.cs
+++ 2-Exceptions/FixedSizeQueue.cs
@@ -20,16 +20,30 @@ namespace Exceptions
         public object GetFirst()
         {
             // TODO ensure objects can only be retrieve if the item is queue is not empty
-            var first = _items[_firstIndex % Capacity];
-            _firstIndex++;
-            return first;
+            if (Count > 0)
+            {
+                var first = _items[_firstIndex % Capacity];
+                _firstIndex++;
+                return first;
+            }
+            else
+            {
+                throw new EmptyQueueException("Queue is empty");
+            }
         }
         
         public void AddLast(object item)
         {
             // TODO ensure objects can only be inserted if the item is queue is not full
-            _items[_lastIndex % Capacity] = item;
-            _lastIndex++;
+            if (Count < Capacity)
+            {
+                _items[_lastIndex % Capacity] = item;
+                _lastIndex++;
+            }
+            else
+            {
+                throw new FullQueueException("Queue is full, can't add any other item.");
+            }
         }
     }
 }
